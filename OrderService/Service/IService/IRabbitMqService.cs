0d43208be07c6d6996702cdeb32dd98c52bec5b9
Chetan Mundle <chetanmundle@smartdatainc.net>
2025-04-01 15:09:26 +0530

    rabit mq

---
+++ OrderService/Service/IService/IRabbitMqService.cs
@@ -0,0 +1,9 @@
+﻿using OrderService.Model;
+
+namespace OrderService.Service.IService
+{
+    public interface IRabbitMqService
+    {
+        Task<bool> SendMessageAsync(AssignDeliveryDto message);
+    }
+}
---
+++ OrderService/Service/RabbitMqService.cs
@@ -0,0 +1,27 @@
+﻿using OrderService.Model;
+using OrderService.Service.IService;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace OrderService.Service
+{
+    public class RabbitMqService : IRabbitMqService
+    {
+        public async Task<bool> SendMessageAsync(AssignDeliveryDto message)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "MicroserviceTest", durable: false, exclusive: false, autoDelete: false,
+                arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "MicroserviceTest", body: body);
+
+            return true;
+        }
+    }
+}
